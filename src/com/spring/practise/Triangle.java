cbdf687641e5e7b3cb3284357f9c467b96422095
Mahesh <maheshreddymallidi01@gmail.com>
2022-10-12 13:32:44 +0530

    Spring

---
+++ src/com/spring/practise/Triangle.java
@@ -0,0 +1,17 @@
+package com.spring.practise;
+
+public class Triangle {
+	private String type;
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void draw() {
+		System.out.println(getType()+" Triangle");
+	}
+}
